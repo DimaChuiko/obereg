733c1987673942bc2f05f10f0b01084ab3608ef5
dimachuiko <dimachuiko@gmail.com>
2020-05-01 08:26:17 +0300

    new file

--- js/slick.js
+++ js/slick.js
@@ -54,7 +54,7 @@
                 customPaging: function(slider, i) {
                     return $('<button type="button" />').text(i + 1);
                 },
-                dots: false,
+                dots: true, // точки активны
                 dotsClass: 'slick-dots',
                 draggable: true,
                 easing: 'linear',
